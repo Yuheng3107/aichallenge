de3b582a43369b419743dbed6bcb9ff5bd9e14b8
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-11 19:02:47 +0800

    Made web server prototype

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from main import main
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('./index.html')
+
+@app.route("/video")
+def play_video():
+    main()
