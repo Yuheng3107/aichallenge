2a46c2d9a61f049710ca121dc4c5d6dcbbf08378
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-12 00:21:59 +0800

    Built working prototype of streaming function

--- app.py
+++ app.py
@@ -1,23 +1,22 @@
-import numpy as np
+
 import cv2
 from flask import Flask, render_template, Response
-
+import globals
 from main import main
 app = Flask(__name__)
 
-def initialise():
-    global img
-    img = np.zeros((720, 1280, 3))
 
-def gen(img):
+
+def gen():
     while True:
-        ret, jpeg = cv2.imencode('.jpeg', img)
+        
+        ret, jpeg = cv2.imencode('.jpeg', globals.img)
         frame = jpeg.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 @app.route('/')
 def index():
-    initialise()
+    globals.initialise()
     return render_template('./index.html')
 
 @app.route('/start')
@@ -27,6 +26,5 @@ def start():
 
 @app.route('/video_feed')
 def video_feed():
-    global img
-    return Response(gen(img),
+    return Response(gen(),
     mimetype='multipart/x-mixed-replace; boundary=frame')
---
+++ globals.py
@@ -0,0 +1,5 @@
+import numpy as np
+
+def initialise():
+    global img
+    img = np.zeros((720, 1280, 3))
