0a341696b567fde88c954f2287095c0a7fc47761
Eyewhy <ivanyapweiwang@gmail.com>
2023-01-12 22:59:33 +0800

    removed globals.mistakes, miscomm

--- globals.py
+++ globals.py
@@ -6,11 +6,9 @@ def initialise():
     global exerciseEnded
     global runSwitch
     global currentExercise
-    global mistakes
 
     exerciseEnded = False
     feedback = ["Please do the exercise stipulated"]
     img = np.zeros((720, 1280, 3))
     runSwitch = True
     currentExercise = 0
-    mistakes = []
