a5ed6f2b6f43a18424dcd6973a4d3c94562a5e45
Eyewhy <ivanyapweiwang@gmail.com>
2023-01-12 22:52:27 +0800

    check if body is in img, organise code, add glb variables

--- globals.py
+++ globals.py
@@ -4,7 +4,13 @@ def initialise():
     global img
     global feedback
     global exerciseEnded
+    global runSwitch
+    global currentExercise
+    global mistakes
 
     exerciseEnded = False
     feedback = ["Please do the exercise stipulated"]
     img = np.zeros((720, 1280, 3))
+    runSwitch = True
+    currentExercise = 0
+    mistakes = []
