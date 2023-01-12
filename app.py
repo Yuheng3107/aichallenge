e24fc70fa25e88aebfc0e083b5c428e1cf74f37e
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-12 18:06:22 +0800

    Wrote giveFeedback function prototype

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ def gen():
         frame = jpeg.tobytes()
         yield (b'--frame\r\n'
                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+               
 @app.route('/')
 def index():
     globals.initialise()
