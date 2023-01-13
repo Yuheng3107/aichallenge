f9f97dc3c250716c6c84c07149acffa7b0e9520e
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-01-13 22:54:37 +0800

    changed feedback text format

--- static/js/app.js
+++ static/js/app.js
@@ -1,38 +1,38 @@
 
-function getFeedback(feedback) {
+function getFeedback(feedback, repCount) {
     fetch(feedback.getAttribute('data-url')).then(response => {
         return response.json();
-        
+
     }).then(data => {
-        // data is the json data
-        console.log("Test")
-        let feedbackText = JSON.stringify(data);
-        feedback.textContent = feedbackText;
+        // data returned from backend is an array
+        console.log(data[0]);
+        repCount.textContent = data[0];
+        feedback.textContent = data.slice(1);
     });
 }
 
-    const startButton = document.querySelector('.start-button');
-    const endButton = document.querySelector('.end-button');
-    const feedback = document.querySelector('#feedback');
-    let started = false;
+const startButton = document.querySelector('.start-button');
+const endButton = document.querySelector('.end-button');
+const repCount = document.querySelector('#rep-count');
+const feedback = document.querySelector('#feedback');
+let started = false;
 
-    startButton.addEventListener('click', (e) => {
-        // runs python script that starts Peekingduck
-        if (!started)
-            {
-            fetch(startButton.getAttribute('data-url'));
-            startButton.style.display = "none";
-            started = true;
-            // updates feedback every second
-            setInterval(getFeedback, 1000, feedback);
-        }
-    });
-    endButton.addEventListener('click', () => {
-        fetch(endButton.getAttribute('data-url'));
-        // adds the feedback to the div which displays it
-        // waits for 2s
-        setTimeout(getFeedback(feedback), 2000);
-    });
+startButton.addEventListener('click', (e) => {
+    // runs python script that starts Peekingduck
+    if (!started) {
+        fetch(startButton.getAttribute('data-url'));
+        startButton.style.display = "none";
+        started = true;
+        // updates feedback every second
+        setInterval(getFeedback, 1000, feedback, repCount);
+    }
+});
+endButton.addEventListener('click', () => {
+    fetch(endButton.getAttribute('data-url'));
+    // adds the feedback to the div which displays it
+    // waits for 2s
+    setTimeout(getFeedback(feedback), 2000);
+});
 
 
 
