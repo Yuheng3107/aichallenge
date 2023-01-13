dfde697c646805a69d2667e9212566fabe3c002b
AppleJem <98686428+AppleJem@users.noreply.github.com>
2023-01-13 16:44:59 +0800

    changed styling to bootstrap

--- static/js/app.js
+++ static/js/app.js
@@ -1,18 +1,23 @@
 
-const startButton = document.querySelector('.start-button');
-const endButton = document.querySelector('.end-button');
-let started = false;
-startButton.addEventListener('click', (e) => {
-    // runs python script that gets peekingduck to run
-    if (!started) {
-        fetch(startButton.getAttribute('data-url'));
-        started = true;
-    }
-    
-});
-endButton.addEventListener('click', () => {
-    fetch(endButton.getAttribute('data-url'));
-});
+window.addEventListener('DOMContentLoaded', (event) => { //Wait for the DOM tree to render out before running allowing interaction
+    const startButton = document.querySelector('.start-button');
+    const endButton = document.querySelector('.end-button');
+    let started = false;
+
+    startButton.addEventListener('click', (e) => {
+        // runs python script that starts Peekingduck
+        if (!started) {
+            fetch(startButton.getAttribute('data-url'));
+            started = true;
+        }
+
+    });
+    endButton.addEventListener('click', () => {
+        fetch(endButton.getAttribute('data-url'));
+    });
+
+})
+
 
 
 
