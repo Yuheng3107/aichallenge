88e74544f9674143d2474f4211218ef749a65cc3
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-13 10:33:31 +0800

    Called eventDefault

--- static/js/app.js
+++ static/js/app.js
@@ -2,5 +2,5 @@
 console.log(document.getElementsByClassName('end-button').item(0));
 
 document.querySelector('.end-button').addEventListener('click', (event) => {
-    event.preventDefault
+    event.preventDefault();
 })
