20c49ab0986e0c78e4fa42d34690bda336829d40
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-13 08:26:07 +0800

    Changed configs so only one pose is detected

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ from peekingduck.runner import Runner
 def main():
     processing_node = correctMain.Node(pkd_base_dir=Path.cwd() / "src" / "custom_nodes")
     visual_node = visual.Node(source=0)
-    posenet_node = posenet.Node()
+    posenet_node = posenet.Node(max_pose_detection=1)
     poses_node = poses.Node()
     
     # screen_node = screen.Node()
