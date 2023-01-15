ae6f6f22d345d17c2e0bb96183cec76b7ec12d1b
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-15 22:11:52 +0800

    Added async code

--- src/custom_nodes/dabble/emotion.py
+++ src/custom_nodes/dabble/emotion.py
@@ -7,8 +7,11 @@ import numpy as np
 import globals
 from deepface import DeepFace
 from peekingduck.pipeline.nodes.abstract_node import AbstractNode
+import asyncio
 
-
+async def detect_emotion():
+    globals.emotion = DeepFace.analyze(globals.img, actions= ['emotion'], enforce_detection=False)
+    print(globals.emotion)
 class Node(AbstractNode):
     """This is a template class of how to write a node for PeekingDuck.
 
@@ -33,8 +36,8 @@ class Node(AbstractNode):
         Returns:
             outputs (dict): Empty Dictionary.
         """
-        globals.emotion = DeepFace.analyze(globals.img, actions= ['emotion'], enforce_detection=False)
-        print(globals.emotion)
+        asyncio.run(detect_emotion())
+        
         # result = do_something(inputs["in1"], inputs["in2"])
         # outputs = {"out1": result}
         # return outputs
