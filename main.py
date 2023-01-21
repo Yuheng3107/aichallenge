9c74b3992d73df1fcecac547e6286f038b386147
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-21 18:04:41 +0800

    Added documentation on threading for visual node

--- main.py
+++ main.py
@@ -12,15 +12,16 @@ from peekingduck.runner import Runner
 def main():
     processing_node = correctMain.Node(pkd_base_dir=Path.cwd() / "src" / "custom_nodes")
     emotion_node = emotion.Node(pkd_base_dir=Path.cwd() / "src" / "custom_nodes")
-    # Change source to file name to parse file
-
+    
+# Change source to file name to parse file
     sources = [[0],
         ["Training_Data/Squats/Good_Pose/Side_View.mp4","Training_Data/Squats/Bad_Pose/Butt/Side_View.mp4"],
         ["Training_Data/Squats/Good_Pose/Front_View1.mp4","Training_Data/Squats/Bad_Pose/Knee/Front_View1.mp4"],
         ["Training_Data/Push_Up/Good_Pose/Side_View.mp4","Training_Data/Push_Up/Bad_Pose1/Side_View.mp4","Training_Data/Push_Up/Bad_Pose2/Side_View.mp4"]]
 
 
-
+    # Add threading=True to arguments in visual.Node constructor to
+    # enable threading
     visual_node = visual.Node(source=sources[1][0])
     posenet_node = posenet.Node(max_pose_detection=1)
     poses_node = poses.Node()
