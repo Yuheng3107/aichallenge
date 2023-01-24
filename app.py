47ec45a1d33ef2f8394be2bc46a78d1a22434aee
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-24 15:27:19 +0800

    Updated app

--- app.py
+++ app.py
@@ -90,6 +90,8 @@ if __name__ == '__main__':
     # ssl_context=('cert.pem', 'key.pem')
     # debug=True
     # allow_unsafe_werkzeug=True
-    # gunicorn -w 1 --threads 100 app:app use this command to run production ready server
-    socketio.run(app,  host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'))
+    # gunicorn -w 1 --threads 100 app:app 
+    # use this command to run production ready server
+    socketio.run(app,  host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'), allow_unsafe_werkzeug=True)
+    
 
