d4fd62bad1c8c61f25766abd48e7fa7e8fccec1d
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-24 15:13:24 +0800

    Enabled Gunicorn to work

--- app.py
+++ app.py
@@ -89,5 +89,7 @@ def handle_video(data):
 if __name__ == '__main__':
     # ssl_context=('cert.pem', 'key.pem')
     # debug=True
-    socketio.run(app,  host="0.0.0.0", allow_unsafe_werkzeug=True, ssl_context=('cert.pem', 'key.pem'))
+    # allow_unsafe_werkzeug=True
+    # gunicorn -w 1 --threads 100 app:app use this command to run production ready server
+    socketio.run(app,  host="0.0.0.0", ssl_context=('cert.pem', 'key.pem'))
 
