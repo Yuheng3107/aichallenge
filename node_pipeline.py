1ba6671eb2ad0b6d2950addd88e36122682c4a9d
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-26 20:56:09 +0800

    Added screen node for funsies

--- node_pipeline.py
+++ node_pipeline.py
@@ -2,15 +2,18 @@ from pathlib import Path
 from peekingduck.pipeline.nodes.model import posenet
 from src.custom_nodes.input import webcam
 from src.custom_nodes.dabble import correctMain
-# from peekingduck.pipeline.nodes.output import media_writer, screen
+from peekingduck.pipeline.nodes.output import media_writer, screen
 from peekingduck.runner import Runner
 
 
 def start_pipeline():
     # Custom Nodes
+    if globals.running:
+        return
+    globals.running = False
     webcam_node = webcam.Node(pkd_base_dir=Path.cwd() / "src" / "custom_nodes")
     processing_node = correctMain.Node(pkd_base_dir=Path.cwd() / "src" / "custom_nodes")
-    
+    screen_node = screen.Node()
 # Change source to file name to parse file
     sources = [[0],
         ["Training_Data/Squats/Good_Pose/Side_View3.mp4","Training_Data/Squats/Bad_Pose/Butt/Side_View.mp4","Training_Data/Squats/Bad_Pose/Knee/Side_View2.mp4"],
@@ -25,7 +28,7 @@ def start_pipeline():
             webcam_node,
             posenet_node,
             processing_node,
-
+            screen_node
         ]
     )
 
