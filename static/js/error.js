dfbd235de3ffc75d3de7cd567b399a39150023a3
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-30 15:44:43 +0800

    Added videos

---
+++ static/js/error.js
@@ -0,0 +1,15 @@
+
+
+const sideSquats = document.querySelector('#side-squats');
+const frontSquats = document.querySelector('#front-squats');
+const sidePushUps = document.querySelector('#side-push-ups');
+
+const sections = [
+    sideSquats,
+    frontSquats,
+    sidePushUps
+];
+
+const videos = [
+    
+]
