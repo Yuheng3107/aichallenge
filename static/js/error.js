dd77bae3eb704b320ca8e5e35e45120fcac2abdf
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-31 12:07:08 +0800

    Modified some stuff

--- static/js/error.js
+++ static/js/error.js
@@ -19,3 +19,33 @@ const navItems = [
     getElementByClass('squat-front-nav-item'),
     getElementByClass('push-up-nav-item')
 ];
+
+// set up observer
+const observerOptions = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.7,
+  };
+
+function observerCallback(entries, observer) {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            // get the nav item corresponding to the id of the section
+            // that is currently in view
+            let navItem = navItems[entry.target.id];
+            // add 'active' class on the navItem
+            navItem.classList.add('active');
+            // remove 'active' class from any navItem that is not
+             // same as 'navItem' defined above
+             Object.values(navItems).forEach(item => {
+                 if (item != navItem) {
+                     item.classList.remove('active');
+                 }
+             });
+        }
+    });
+}
+
+const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+sections.forEach(section => observer.observe(section));
