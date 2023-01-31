c76dd13d865a639eccc4fa863c6d4065b217e37d
Kuang Yu Heng <yuheng3107@gmail.com>
2023-01-31 13:00:46 +0800

    Added scroll aware content

--- static/js/error.js
+++ static/js/error.js
@@ -13,39 +13,39 @@ const sections = [
     getElementById('push-up')
 ];
 
-const navItems = [
-    getElementByClass('rep-time-nav-item'),
-    getElementByClass('squat-side-nav-item'),
-    getElementByClass('squat-front-nav-item'),
-    getElementByClass('push-up-nav-item')
-];
+const navItems = {
+    'rep-time': getElementByClass('rep-time-nav-item'),
+    'squats-side': getElementByClass('squat-side-nav-item'),
+    'squats-front': getElementByClass('squat-front-nav-item'),
+    'push-up': getElementByClass('push-up-nav-item')
+};
 
 // set up observer
 const observerOptions = {
     root: null,
     rootMargin: '0px',
-    threshold: 0.7,
+    threshold: [0.1, 0.1, 0.1],
   };
 
-function observerCallback(entries, observer) {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            // get the nav item corresponding to the id of the section
-            // that is currently in view
-            let navItem = navItems[entry.target.id];
-            // add 'active' class on the navItem
-            navItem.classList.add('active');
-            // remove 'active' class from any navItem that is not
-             // same as 'navItem' defined above
-             Object.values(navItems).forEach(item => {
-                 if (item != navItem) {
-                     item.classList.remove('active');
-                 }
-             });
-        }
+  function observerCallback(entries, observer) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        // get the nav item corresponding to the id of the section
+        // that is currently in view
+        const navItem = navItems[entry.target.id];
+        // add 'active' class on the navItem
+        navItem.classList.add('active');
+        // remove 'active' class from any navItem that is not
+        // same as 'navItem' defined above
+        Object.values(navItems).forEach((item) => {
+          if (item != navItem) {
+            item.classList.remove('active');
+          }
+        });
+      }
     });
-}
+  }
 
 const observer = new IntersectionObserver(observerCallback, observerOptions);
 
-sections.forEach(section => observer.observe(section));
+sections.forEach((sec) => observer.observe(sec));
